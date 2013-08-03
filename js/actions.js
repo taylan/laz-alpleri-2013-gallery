0e5f2307a29088331a48461c0de159fd31507183
Taylan Aydinli <taylanaydinli@gmail.com>
2013-08-03 14:19:18 +0300

    Defined all paths and locations

---
+++ js/actions.js
@@ -0,0 +1,98 @@
+var actions = [
+    {
+        key: "oberj-1",
+        name: "Oberj",
+        type: "Stay",
+        location: { lat: 40.951111, lon: 41.109319 }
+    },
+    {
+        key: "osmanli-alabalik",
+        name: "Osmanlı Alabalık",
+        type: "Stay",
+        location: { lat: 41.122675, lon: 41.039939 }
+    },
+    {
+        key: "camlihemsin-komati",
+        name: "Çamlıhemşin -> Komati",
+        type: "Travel",
+        path: paths["camlihemsin-komati"]
+    },
+    {
+        key: "komati",
+        name: "Komati",
+        type: "Stay",
+        location: { lat: 41.030008, lon: 41.214519 }
+    },
+    {
+        key: "komati-yuruyus",
+        name: "Komati Yürüyüşü",
+        type: "Travel",
+        path: paths["komati-yuruyus"]
+    },
+    {
+        key: "komati-yuruyusu-yayla-evi",
+        name: "Komati Yürüyüşü Yayla Evi",
+        type: "Stay",
+        location: { lat: 41.001142, lon: 41.252736 }
+    },
+    {
+        key: "komati-didingola",
+        name: "Komati -> Didingola",
+        type: "Travel",
+        path: paths["komati-didingola"]
+    },
+    {
+        key: "didingola",
+        name: "Didingola",
+        type: "Stay",
+        location: { lat: 40.990778, lon: 41.220475 }
+    },
+    {
+        key: "didingola-adali-gol",
+        name: "Didingola Adalı Göl Yürüyüşü",
+        type: "Travel",
+        path: paths["didingola-adali-gol"]
+    },
+    {
+        key: "adali-gol-pansiyon",
+        name: "Adalı Göl Pansiyon",
+        type: "Stay",
+        location: { lat: 41.003611, lon: 41.192222 }
+    },
+    {
+        key: "didingola-dadala",
+        name: "Didingola -> Dadala",
+        type: "Travel",
+        path: paths["didingola-dadala"]
+    },
+    {
+        key: "buyuk-gol",
+        name: "Büyük Göl",
+        type: "Stay",
+        location: { lat: 40.969861, lon: 41.222703 }
+    },
+    {
+        key: "dadala",
+        name: "Dadala",
+        type: "Stay",
+        location: { lat: 40.962933, lon: 41.224633 }
+    },
+    {
+        key: "dadala-avusor",
+        name: "Dadala -> Avusor",
+        type: "Travel",
+        path: paths["dadala-avusor"]
+    },
+    {
+        key: "avusor",
+        name: "Avusor",
+        type: "Stay",
+        location: { lat: 40.950892, lon: 41.189094 }
+    },
+    {
+        key: "oberj-2",
+        name: "Oberj",
+        type: "Stay",
+        location: { lat: 40.951111, lon: 41.109319 }
+    },
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,77 @@
-$(document).ready(function(){
-    google.maps.visualRefresh = true;
-    map = new google.maps.Map(document.getElementById("map-container"), {
-            disableDefaultUI: true,
-            disableDoubleClickZoom: true,
-            scrollwheel: true,
-            keyboardShortcuts: false,
-            minZoom: 4,
-            zoom: 11,
-            mapTypeId: google.maps.MapTypeId.SATELLITE,
-            zoomControl: true,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.SMALL
-            },
-            mapTypeControl: true,
-            mapTypeControlOptions: {
-                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-            },
-            overviewMapControl: true
+var mappe;
+function LazAlpleriController($scope){
+    $scope.actions = actions;
+    $scope.markers = [];
+    $scope.paths = [];
+
+    initializeMapPoints = function() {
+        angular.forEach($scope.actions, function(action){
+            if(action.type == "Stay") {
+                var marker = new google.maps.Marker({
+                    map: $scope.map,
+                    position: new google.maps.LatLng(action.location.lat, action.location.lon),
+                    animation: google.maps.Animation.DROP,
+                    title: action.name,
+                    key: action.key
+                });
+
+                $scope.markers.push(marker);
+            }
+            else {
+                var path = new google.maps.Polyline({
+                    map: $scope.map,
+                    path: action.path,
+                    strokeColor: "#FF0000",
+                    strokeOpacity: 0.7,
+                    strokeWeight: 2
+                  });
+
+                $scope.paths.push(path);
+            }
         });
-    map.setCenter(new google.maps.LatLng(41, 29));
+    };
+
+    initializeMap = function() {
+        google.maps.visualRefresh = true;
+        //return true;
+        $scope.map = new google.maps.Map(document.getElementById("map-container"), {
+                disableDefaultUI: true,
+                disableDoubleClickZoom: true,
+                scrollwheel: true,
+                keyboardShortcuts: false,
+                minZoom: 5,
+                zoom: 11,
+                mapTypeId: google.maps.MapTypeId.HYBRID,
+                zoomControl: true,
+                zoomControlOptions: {
+                    style: google.maps.ZoomControlStyle.SMALL
+                },
+                mapTypeControl: true,
+                mapTypeControlOptions: {
+                    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+                },
+                overviewMapControl: true
+            });
+
+        $scope.map.setCenter(new google.maps.LatLng(41.047891, 41.149128));
+        mappe = $scope.map;
+    };
+
+
+
+    initializeMap();
+    initializeMapPoints();
+}
+
+$(document).ready(function(){
+
+
+  //   var gun1Path = new google.maps.Polyline({
+  //   path: gun1Points,
+  //   strokeColor: "#FF0000",
+  //   strokeOpacity: 1.0,
+  //   strokeWeight: 2
+  // });
+
+  // gun1Path.setMap(map);
 });
